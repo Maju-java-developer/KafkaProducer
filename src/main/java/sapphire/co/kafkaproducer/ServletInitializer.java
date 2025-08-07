61b678b274f42741e765b39d993dd6b3dcdd3c19
Majid Hussain <122609422+Maju-java-developer@users.noreply.github.com>
2025-08-07 12:24:16 +0500

    test space

--- src/main/java/sapphire/co/kafkaproducer/ServletInitializer.java
+++ src/main/java/sapphire/co/kafkaproducer/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        System.out.println("Application started");
         return application.sources(KafkaProducerApplication.class);
     }
 
